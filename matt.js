527cf7c54580b5798d2257e1b788238cb6ef26ca
benjamin lees <benji.man.lees@gmail.com>
2015-10-07 13:50:20 +0100

    small add to the document

--- matt.js
+++ matt.js
@@ -1,16 +1,43 @@
 
-var styleArray = ["matt","matt1","matt2","matt3","matt4","matt5"]
+var styleArray = ["matt","matt1","matt2","matt3","matt4","matt5"];
 
-document.onkeydown = function () {
+var bold = false;
+var spacing = false;
 
-	setTimeout(function () {
-		var text = document.getElementById("text").value;
-		var letter = text[text.length - 1];
-		var randomNum = Math.random() * 5;
+document.onkeydown = function (e) {
 
-		randomNum = Math.round(randomNum);
+		setTimeout(function () {
 
-		document.getElementById("out-put").innerHTML += "<span class = " + styleArray[randomNum] + ">" + letter + "</span>";
-	}, 1)
+			if (e.keyCode === 8) {
+				var doc = document.getElementById("out-put").getElementsByTagName('span')
+				var elem = doc[doc.length -1];
+				document.getElementById("out-put").removeChild(elem);
+				return;
+			}
+			if (e.keyCode === 13) {
+				document.getElementById("out-put").innerHTML += "<br />";
+				return;
+			}
+			if (document.getElementById("text").value) {7
+				var style = '';
+				var text = document.getElementById("text").value;
+				var letter = text[text.length - 1];
+				var randomNum = Math.random() * 5;
 
+				randomNum = Math.round(randomNum);
+
+				if (bold && spacing) { style = "bold spacing "};
+				if (bold && !spacing) { style = "bold "};
+				if (!bold && spacing) { style = "spacing "};
+
+				style = style + styleArray[randomNum];
+				console.log(style)
+
+				document.getElementById("out-put").innerHTML += "<span class = '" + style + "'>" + letter + "</span>";
+
+			}
+		}, 1);
 }
+
+document.getElementById("bold").addEventListener('click', function () { bold = !bold; });
+document.getElementById("spacing").addEventListener('click', function () { spacing = !spacing; });
